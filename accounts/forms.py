a8dd44f744ed347bee871ec89d129dc2c155e2a2
leowu0329 <ryowuandjanet@email.com>
2025-02-08 00:52:06 +0800

    微調-part1

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = CustomUser
-        fields = (
-            "username",
-            "email",
-            "age",
-        )  # new
+        model = get_user_model()
+        fields = UserCreationForm.Meta.fields + ("email",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 自定義錯誤訊息
+        self.error_messages['password_mismatch'] = _('兩次輸入的密碼不一致')
 
 
 class CustomUserChangeForm(UserChangeForm):
--- articles/models.py
+++ articles/models.py
@@ -19,16 +19,18 @@ class Article(models.Model):
         return reverse("article_detail", kwargs={"pk": self.pk})
 
 
-class Comment(models.Model):  # new
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+class Comment(models.Model):
+    article = models.ForeignKey("Article", on_delete=models.CASCADE)
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.comment
 
     def get_absolute_url(self):
-        return reverse("article_list")
+        return reverse("article_detail", kwargs={"pk": self.article.pk})
